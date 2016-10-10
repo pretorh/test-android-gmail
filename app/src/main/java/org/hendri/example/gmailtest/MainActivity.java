2db409fc6a8bc909c93b61e212a5157b465f7ce5
Hendri Pretorius <pretorh@gmail.com>
2016-10-10 11:25:17 +0200

    import code form quickstart
    https://developers.google.com/gmail/api/quickstart/android

--- app/src/main/java/org/hendri/example/gmailtest/MainActivity.java
+++ app/src/main/java/org/hendri/example/gmailtest/MainActivity.java
@@ -1,13 +1,250 @@
 package org.hendri.example.gmailtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    private static final String[] SCOPES = { GmailScopes.GMAIL_LABELS };
+    private GoogleAccountCredential googleAccountCredential;
+    private TextView txtResult;
+
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        setupButton();
+
+        googleAccountCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
+    }
+
+    private void setupButton() {
+        final View button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setEnabled(false);
+                getResultFromApi();
+                button1.setEnabled(true);
+            }
+        });
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                Log.d(TAG, "onActivityResult: REQUEST_GOOGLE_PLAY_SERVICES");
+                if (resultCode != RESULT_OK) {
+                    txtResult.setText("This app requires Google Play Services. Please install Google Play Services on your device and relaunch this app.");
+                } else {
+                    getResultFromApi();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                Log.d(TAG, "onActivityResult: REQUEST_ACCOUNT_PICKER");
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
+                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        saveSelectedAccountInPreferences(accountName);
+                        googleAccountCredential.setSelectedAccountName(accountName);
+                        getResultFromApi();
+                    }
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                Log.d(TAG, "onActivityResult: REQUEST_AUTHORIZATION");
+                if (resultCode == RESULT_OK) {
+                    getResultFromApi();
+                }
+                break;
+        }
+    }
+
+    private void saveSelectedAccountInPreferences(String accountName) {
+        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(PREF_ACCOUNT_NAME, accountName);
+        editor.apply();
+    }
+
+    private void getResultFromApi() {
+        if (!isGooglePlayServicesAvailable()) {
+            acquireGooglePlayServices();
+        } else if (googleAccountCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else if (!isDeviceOnline()) {
+            txtResult.setText("No network connection available.");
+        } else {
+            new MakeRequestTask(googleAccountCredential).execute();
+        }
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        final int connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        return connectionStatusCode == ConnectionResult.SUCCESS;
+    }
+
+    private void acquireGooglePlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        final int connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            Dialog dialog = apiAvailability.getErrorDialog(
+                    MainActivity.this,
+                    connectionStatusCode,
+                    REQUEST_GOOGLE_PLAY_SERVICES);
+            dialog.show();
+        }
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void chooseAccount() {
+        boolean hasPermissions = EasyPermissions.hasPermissions(this, Manifest.permission.GET_ACCOUNTS);
+        if (hasPermissions) {
+            String accountName = getPreferences(Context.MODE_PRIVATE).getString(PREF_ACCOUNT_NAME, null);
+
+            if (accountName != null) {
+                googleAccountCredential.setSelectedAccountName(accountName);
+                getResultFromApi();
+            } else {
+                startActivityForResult(googleAccountCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            EasyPermissions.requestPermissions(this, "This app needs to access your Google account (via Contacts).",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
+        private com.google.api.services.gmail.Gmail gmailService = null;
+        private Exception mLastError = null;
+
+        MakeRequestTask(GoogleAccountCredential credential) {
+            HttpTransport transport = AndroidHttp.newCompatibleTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            gmailService = new com.google.api.services.gmail.Gmail.Builder(
+                    transport, jsonFactory, credential)
+                    .setApplicationName("GmailTest")
+                    .build();
+        }
+
+        @Override
+        protected List<String> doInBackground(Void... params) {
+            try {
+                return getDataFromApi();
+            } catch (Exception e) {
+                mLastError = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        private List<String> getDataFromApi() throws IOException {
+            List<String> labels = new ArrayList<>();
+            ListLabelsResponse listResponse = gmailService.users().labels().list("me").execute();
+            for (Label label : listResponse.getLabels()) {
+                labels.add(label.getName());
+            }
+            return labels;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            txtResult.setText("loading...");
+        }
+
+        @Override
+        protected void onPostExecute(List<String> output) {
+            if (output == null || output.size() == 0) {
+                txtResult.setText("No results returned.");
+            } else {
+                txtResult.setText(TextUtils.join("\n", output));
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            if (mLastError == null) {
+                txtResult.setText("Request cancelled.");
+                return;
+            }
+
+            if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+                int connectionStatusCode = ((GooglePlayServicesAvailabilityIOException) mLastError).getConnectionStatusCode();
+                apiAvailability.getErrorDialog(MainActivity.this, connectionStatusCode, REQUEST_GOOGLE_PLAY_SERVICES).show();
+            } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                startActivityForResult(((UserRecoverableAuthIOException) mLastError).getIntent(), MainActivity.REQUEST_AUTHORIZATION);
+            } else {
+                txtResult.setText("Error: " + mLastError.getMessage());
+            }
+        }
     }
 }
